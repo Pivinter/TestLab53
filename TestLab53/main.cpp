0528bb3af20db6fcf530c97b371ff5280f6ccac8
Pivinter <70973362+Pivinter@users.noreply.github.com>
2022-10-12 22:56:36 +0300

    Add project files.

---
+++ TestLab53/main.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+double f(const double x);
+int main()
+{
+	double xp, xk, z;
+	int n;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "n = "; cin >> n;
+	double dg = (xk - xp) / n;
+	double t = xp;
+	while (t <= xk)
+	{
+		z = f(t * t) + 2*f(2*t+1);
+		cout << t << " " << z << endl;
+		t += dg;
+	}
+	return 0;
+}
+double f(const double x)
+{
+	if (abs(x) >= 1)
+		return (sin(x) + 1) / (sin(x)+cos(x));
+	else
+	{
+		double S = 0;
+		int i = 0;
+		double a = 1;
+		S = a;
+		do
+		{
+			i++;
+			double R = pow(x, 2*i+1) / (((2*i+1)) * ((2 * i + 1) - 1) * (2 * i + 1)* (2 * i + 1)-2);
+			a *= R;
+			S += a;
+		} while (i < 8);
+		return S;
+	}
+}
+
---
+++ UnitTest53u/UnitTest53u.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest53u
+{
+	TEST_CLASS(UnitTest53u)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
